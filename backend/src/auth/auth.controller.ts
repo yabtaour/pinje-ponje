007e753960c779baa61893aaa226d1b3290e2a1e
Youness Abtaouri <younessabtaouri00@gmail.com>
2023-07-11 10:03:05 +0100

    after succesful authentication the user is redirected to his profile

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,15 +1,22 @@
-import { Controller, Get, Post, UseGuards, Res, Req, Redirect } from '@nestjs/common';
-// import { AuthService } from './auth.service';
+import { Controller, Get, Post, UseGuards, Res, Req, Redirect, Param } from '@nestjs/common';
+import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
 import { Response, Request } from 'express';
 import { get } from 'http';
+import { UserService } from 'src/user/user.service';
 
 @Controller('auth')
 export class AuthController {
-    // constructor(private readonly authService: AuthService) {}
+    constructor(
+        // private readonly authService: AuthService,
+        private readonly userService: UserService
+    ) {}
 
-    @Get('42/200')
-    async getProfile() {
+    @Get('42/200/:id')
+    async getProfile(@Param() id) {
+        console.log(id.id);
+        const profile = this.userService.FindUserByID(Number(id.id));
+        return profile;
         return ("jesus loves u <3");
     }
 
@@ -21,10 +28,10 @@ export class AuthController {
     @UseGuards(AuthGuard('42'))
     async handle42Auth(@Req() request: any, @Res() response: Response) {
         console.log("returned here");
-        // const user = request.user;
-        console.log(request.user.token);
+        const user = request.user;
+        console.log(request.user);
         response.setHeader("Authorisation", request.user.token);
-        response.redirect('200');
+        response.redirect(`200/${String(request.user.user.id)}`);
     }
 }
 
