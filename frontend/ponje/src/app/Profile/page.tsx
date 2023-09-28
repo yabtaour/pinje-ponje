e81fd959f8933ffad7a739209ed48a1b81d58fdb
SoukainaSabbaji <sabbajisoukaina@gmail.com>
2023-09-28 21:31:47 +0100

    working on profile components

---
+++ frontend/ponje/src/app/Profile/page.tsx
@@ -0,0 +1,15 @@
+import MatchHistory from "../components/MatchHistory";
+import { ProgressBar } from "../components/ProgressBar";
+import { PlayerBanner } from "../components/PlayerBanner";
+
+
+
+export default function Profile() {
+    return (
+        <div>
+            {/* <MatchHistory /> */}
+            {/* <ProgressBar />  */}
+            <PlayerBanner />
+        </div>
+    )
+}
--- frontend/ponje/src/app/components/PlayerBanner.tsx
+++ frontend/ponje/src/app/components/PlayerBanner.tsx
@@ -1,25 +1,34 @@
 import React from "react";
 import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
 
 export const PlayerBanner = () => {
-  return (
-    <div className="relative">
-      <Image
-        src="/PlayerBanner.png"
-        alt="banner image"
-        width={1920}
-        height={400}
-      />
-      <Image
-        src="/TestUserImage.png"
-        alt="user image"
-        width={80}
-        height={80}
-        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-      />
-      <p className="absolute bottom-0 left-1/2 transform -translate-x-1/2">
-        This is some text under the user image
-      </p>
-    </div>
-  );
+    return (
+        <div className="relative">
+            <Image
+                src="/PlayerBanner.png"
+                alt="banner image"
+                width={1920}
+                height={400}
+            />
+            <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 text-center w-full">
+                <Image
+                    src="/TestUserImage.png"
+                    alt="user image"
+                    width={80}
+                    height={80}
+                    className="mx-auto"
+                />
+                <p className="font-medium text-[#77DFF8]">ssabbaji</p>
+                <p className="font-light text-sm text-[#8C8CDA]">
+                    soukaina | Joined Sept 28 2023
+                </p>
+                <button className="btn btn-sm btn-active btn-primary mt-4">
+                    <FontAwesomeIcon icon={faUserPlus} className="mr-2" style={{ fontSize: "0.8em" }} />
+                    Add Friend
+                </button>
+            </div>
+        </div>
+    );
 };
