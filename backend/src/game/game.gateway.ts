02d0cc80a9772c19211fd9309a80a191e15a252c
Youness Abtaouri <younessabtaouri00@gmail.com>
2023-11-19 16:07:16 +0000

    adding protections to events payloads

--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -1,9 +1,9 @@
-import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer, WsException } from '@nestjs/websockets';
 import { Server } from 'socket.io';
 import { AuthWithWs } from 'src/chat/dto/user-ws-dto';
 import { GameState } from './gameState';
 import { GameService } from './game.service';
-import { Inject, forwardRef } from '@nestjs/common';
+import { ArgumentMetadata, Inject, Injectable, PipeTransform, UsePipes, forwardRef } from '@nestjs/common';
 import { IsNotEmpty, IsNumber } from 'class-validator';
 import { UpdateBallPositionDto, UpdatePaddlePositionDto, UpdateScoreDto } from './dto/game.dto';
 
@@ -13,6 +13,27 @@ import { UpdateBallPositionDto, UpdatePaddlePositionDto, UpdateScoreDto } from '
     origin: '*'
   }
 })
+
+@Injectable()
+export class PayloadValidationPipe implements PipeTransform<any> {
+  transform(value: any, metadata: ArgumentMetadata): any {
+    if (!this.isValidPayload(value)) {
+      throw new WsException('Invalid payload');
+    }
+    return value;
+  }
+
+	private isValidPayload(payload: UpdatePaddlePositionDto): boolean {
+    if (!payload) {
+			return false;
+		}
+		if (payload.gameId === undefined || payload.gameId === null
+			|| payload.direction === undefined || payload.direction === null) {
+			return false;
+		}
+    return true;
+	}
+}
 
 export class GameGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
 
@@ -43,6 +64,7 @@ export class GameGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
     // }
 
     @SubscribeMessage('updatePlayerPosition')
+		// @UsePipes(new PayloadValidationPipe())
     updatePlayerPosition(client: any, payload: UpdatePaddlePositionDto): void {
       console.log(client);
       this.gameService.updatePlayerPosition(client.id, payload);
@@ -68,6 +90,8 @@ export class GameGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
     
     async handleConnection(client: AuthWithWs) {
       const sockets = this.server.sockets;
+			console.log(`Client connected: ${client.id}`);
+			console.log(this.server);
       // const rooms = await this.gameService.getRooms();
     }
     
