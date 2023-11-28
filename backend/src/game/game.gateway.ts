b3478218394b8c59bb80ca0efe2c974c58651cfb
Youness Abtaouri <--global>
2023-11-28 10:51:40 +0100

    updated gateways to use new global exceptions filter

--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -3,38 +3,38 @@ import { Server } from 'socket.io';
 import { AuthWithWs } from 'src/chat/dto/user-ws-dto';
 import { GameState } from './gameState';
 import { GameService } from './game.service';
-import { ArgumentMetadata, Inject, Injectable, PipeTransform, UsePipes, forwardRef } from '@nestjs/common';
+import { ArgumentMetadata, Inject, Injectable, PipeTransform, UseFilters, UsePipes, forwardRef } from '@nestjs/common';
 import { IsNotEmpty, IsNumber } from 'class-validator';
 import { UpdateBallPositionDto, UpdatePaddlePositionDto, UpdateScoreDto } from './dto/game.dto';
+import { GlobalExceptionFilter } from 'src/global-exception.filter';
 
+// export class PayloadValidationPipe implements PipeTransform<any> {
+//   transform(value: any, metadata: ArgumentMetadata): any {
+//     if (!this.isValidPayload(value)) {
+//       throw new WsException('Invalid payload');
+//     }
+//     return value;
+//   }
+
+// 	private isValidPayload(payload: UpdatePaddlePositionDto): boolean {
+//     if (!payload) {
+// 			return false;
+// 		}
+// 		if (payload.gameId === undefined || payload.gameId === null
+// 			|| payload.direction === undefined || payload.direction === null) {
+// 			return false;
+// 		}
+//     return true;
+// 	}
+// }
+@UseFilters(new GlobalExceptionFilter())
 @WebSocketGateway({
   namespace: 'game',
   cors: {
     origin: '*'
   }
 })
-
 @Injectable()
-export class PayloadValidationPipe implements PipeTransform<any> {
-  transform(value: any, metadata: ArgumentMetadata): any {
-    if (!this.isValidPayload(value)) {
-      throw new WsException('Invalid payload');
-    }
-    return value;
-  }
-
-	private isValidPayload(payload: UpdatePaddlePositionDto): boolean {
-    if (!payload) {
-			return false;
-		}
-		if (payload.gameId === undefined || payload.gameId === null
-			|| payload.direction === undefined || payload.direction === null) {
-			return false;
-		}
-    return true;
-	}
-}
-
 export class GameGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
 
   @WebSocketServer()
@@ -44,8 +44,6 @@ export class GameGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
   constructor(
 		@Inject(forwardRef(() => GameService))
 		private gameService: GameService,
-    // private  gameService: GameService,
-    // logger: Logger = new Logger('GameGateway')
   ) {}
   
     afterInit(server: Server) {
@@ -58,23 +56,12 @@ export class GameGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
       return 'Hello world!';
     }
 
-    // @SubscribeMessage('spectate')
-    // spectate(client: any, payload: {userId: number, gameId: number}): void {
-    //   this.spectators.set(payload.gameId, payload.userId);
-    // }
-
     @SubscribeMessage('updatePlayerPosition')
-		// @UsePipes(new PayloadValidationPipe())
     updatePlayerPosition(client: any, payload: UpdatePaddlePositionDto): void {
       console.log(client);
       this.gameService.updatePlayerPosition(client.id, payload);
     }
   
-    // @SubscribeMessage('updateBallPosition')
-    // updateBallPosition(client: any, payload: UpdateBallPositionDto): void {
-    //   this.gameService.updateBallPosition(payload);
-    // }
-
     @SubscribeMessage('gameOver')
     gameOver(client: any, payload: {gameId: number}): void {
       this.gameService.gameOver(payload.gameId);
@@ -83,16 +70,14 @@ export class GameGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
     @SubscribeMessage('updateScore')
     updateScore(client: any, payload: UpdateScoreDto): void {
       console.log(payload);
+      console.log(client);
       this.gameService.updateScore(payload.userId, payload.gameId);
     }
-    // @SubscribeMessage('game')
-    // handleMessage(client: any, payload:)
-    
+
     async handleConnection(client: AuthWithWs) {
       const sockets = this.server.sockets;
 			console.log(`Client connected: ${client.id}`);
 			console.log(this.server);
-      // const rooms = await this.gameService.getRooms();
     }
     
     handleDisconnect(client: AuthWithWs) {
--- backend/src/global-exception.filter.ts
+++ backend/src/global-exception.filter.ts
@@ -3,41 +3,6 @@ import { WsException } from '@nestjs/websockets';
 import e, { Request, Response } from 'express';
 import { JsonWebTokenError } from 'jsonwebtoken';
 
-// @Catch()
-// export class GlobalExceptionFilter implements ExceptionFilter {
-//   catch(exception: HttpException | WsException, host: ArgumentsHost) {
-//     Logger.error(exception);
-//     const response = host.switchToHttp().getResponse<Response>();
-//     const request = host.switchToHttp().getRequest<Request>();
-
-//     let status = HttpStatus.INTERNAL_SERVER_ERROR;
-//     let message = 'Internal server error';
-
-//     if (exception instanceof JsonWebTokenError) {
-//       status = HttpStatus.UNAUTHORIZED;
-//       message = exception.message;
-//     }
-
-//     if (exception instanceof HttpException) {
-//       status = exception.getStatus();
-//       message = exception.message;
-//     }
-
-//     if (exception instanceof WsException) {
-//       status = HttpStatus.INTERNAL_SERVER_ERROR;
-//       message = exception.message;
-//     }
-    
-//     Logger.error(`Error ${status} ${message}`);
-//     response.status(status).json({
-//       statusCode: status,
-//       message: message,
-//       timestamp: new Date().toISOString(),
-//       path: request.url,
-//     });
-//   }
-// }
-
 @Catch()
 export class GlobalExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException | WsException, host: ArgumentsHost) {
@@ -68,6 +33,7 @@ export class GlobalExceptionFilter implements ExceptionFilter {
     }
 
     else if (exception instanceof HttpException) {
+      Logger.error(exception);
       const response = host.switchToHttp().getResponse<Response>();
       const request = host.switchToHttp().getRequest<Request>();
 
@@ -84,6 +50,7 @@ export class GlobalExceptionFilter implements ExceptionFilter {
     }
 
     else {
+      Logger.error(exception);
       const response = host.switchToHttp().getResponse<Response>();
       const request = host.switchToHttp().getRequest<Request>();
 
