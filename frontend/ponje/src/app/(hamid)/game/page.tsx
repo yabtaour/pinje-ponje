b00f5d8291cfef83136830d286440888bf58130a
Youness Abtaouri <--global>
2023-11-30 16:53:40 +0100

    started rendering the game

--- frontend/ponje/src/app/(hamid)/game/page.tsx
+++ frontend/ponje/src/app/(hamid)/game/page.tsx
@@ -1,123 +1,127 @@
-// 'use client';
-// import React, { useEffect } from 'react';
-// import { Engine, Render, World, Bodies, Body, Events } from 'matter-js';
 
-// const PingPongGame: React.FC = () => {
-//     useEffect(() => {
-//     // Matter.js setup goes here
-//     const engine = Engine.create();
-//     const render = Render.create({
-//         element: document.getElementById('gameContainer')!,
-//         engine: engine,
-//     });
-//     const World = engine.world;
-//     const ball = Bodies.circle(400, 300, 20, {
-//         restitution: 0.8,
-//         friction: 0,
-//         render: {
-//             fillStyle: '#ffffff',
-//             },
-//         });
-//     });
-//     const rectangle = Bodies.rectangle(400, 500, 80, 80);
-//     return () => {
-//         Render.stop(render);
-//         Engine.clear(engine);
-//         World.remove(World, ball);
-//     };
-// }, []); // Run once on component mount
-
-//     // return (
-//     //     <div id="gameContainer" style={{ width: '800px', height: '600px', margin: 'auto' }}>
-//     //     {/* The canvas will be rendered here */}
-//     //     </div>
-//     // );
-// // }
-
-// export default PingPongGame;
 'use client';
-import { Bodies, Engine, Render, World } from 'matter-js';
-import { useEffect } from 'react';
+import Matter, { Body, Events } from 'matter-js';
+import { useEffect, useRef } from 'react';
 
-export default function PingPongGame() {
+export const Engine = Matter.Engine;
+export const Render = Matter.Render;
+export const World = Matter.World;
+export const Bodies = Matter.Bodies;
+export const Runner = Matter.Runner;
+
+let ball: Matter.Body;
+let floor: Matter.Body;
+let ceiling: Matter.Body;
+let rightPaddle: Matter.Body;
+let leftPaddle: Matter.Body;
+
+export function createBodies(canvaWidth: number, canvaHeight: number) {
+  ball = Bodies.circle(canvaWidth / 2, canvaHeight / 2, 10, { 
+    restitution: 1,
+    frictionAir: 0,
+    // isStatic: true,
+  });
+  Body.setVelocity(ball, {
+    x: 0,
+    y: 5
+  })
+  rightPaddle = Bodies.rectangle(canvaWidth - 10, canvaHeight / 2, 10, 100);
+  leftPaddle = Bodies.rectangle(10, canvaHeight / 2, 10, 100);
+  floor = Bodies.rectangle(0, canvaHeight, canvaWidth * 2, 5, { isStatic: true });
+  ceiling = Bodies.rectangle(0, 0, canvaWidth * 2, 5, { isStatic: true });
+  return ([rightPaddle, leftPaddle, ball, floor, ceiling])
+}
+
+export function handleColision(bodyA: Matter.Body, bodyB: Matter.Body) {
+  // [ball, floor, ceiling, rightPaddle, leftPaddle] = worlBodies;
+
+  console.log("hnaaaaaaa")
+
+  if (bodyA == ball || bodyB == ball) {
+    console.log("it's a ball")
+    if ((bodyA == floor || bodyB == floor)) {
+      console.log(ball);
+      Body.setVelocity(ball, {
+        x: 0,
+        y: 6
+      })
+      console.log("floor collision")
+    } else if ((bodyA === ceiling || bodyB === ceiling)) {
+      console.log(ball);
+      Body.setVelocity(ball, {
+        x: 0,
+        y: 6
+      })
+      // ballOnCeiling = true;
+    }
+
+    if (bodyA === ceiling || bodyB === ceiling) {
+      ball.velocity.x = 0;
+      ball.velocity.y = 0;
+    }
+  }
+}
+
+export default function Game() {
+  const boxRef = useRef<HTMLDivElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
   useEffect(() => {
-    // Matter.js setup goes here
-    const containerStyles = getComputedStyle(document.getElementById('gameContainer')!);
-    console.log("containerStyles",containerStyles);
-    console.log("containerStyles",containerStyles.width);
-    console.log("containerStyles",containerStyles.height);
-    const engine = Engine.create();
+
+    const worldWidth = canvasRef.current?.width! * 2;
+    const worldHeight = canvasRef.current?.height! * 2;
+    const engine = Engine.create({
+      gravity: {
+        x: 0,
+        y: 0,
+      }
+    });
     const render = Render.create({
-      element: document.getElementById('gameContainer')!,
+      element: boxRef.current!,
+      canvas: canvasRef.current!,
       engine: engine,
       options: {
-        // gravity: {
-        //     x: 0,
-        //     y: 0,
-        // },
-        // width: Number(containerStyles.width),
-        // wireframeBackground: 'transparent',
-        // width: (window.innerWidth / 3) + (window.innerWidth / 3),
-        // width: 800,
-        // height: parseFloat(containerStyles.height) - 200,
-        // height: 600,
-        // height: window.innerHeight / 5 ,
-        // wireframes: false,
-        // width: canvasWidth,
-        background: 'transparent',
+        width: worldWidth,
+        height: worldHeight,
+        wireframes: false,
+        background: '',
       },
     });
+    const canvaWidth = canvasRef.current?.width!;
+    const canvaHeight = canvasRef.current?.height!;
+    const worlBodies = createBodies(canvaWidth, canvaHeight)
+    World.add(engine.world, [ball, floor, ceiling, leftPaddle, rightPaddle]);
+    // [ball, floor, ceiling, rightPaddle, leftPaddle] = worlBodies;
 
-    const world = engine.world;
-    console.log("hamid",world);
-    // console.log(world);
-
-    const ball = Bodies.circle(400, 300, 20, {
-        restitution: 0.8,
-        friction: 0,
-        render: {
-            fillStyle: '#000',
-        },
-    });
-    const rectangle1 = Bodies.rectangle(200, 20, 20, 20);
-    const rectangle2 = Bodies.rectangle(20, 20, 20, 20);
-
-    // Add bodies to the world
-    World.add(world, [ball, rectangle1, rectangle2]);
-// 
-    // Add the renderer to the page
     Engine.run(engine);
     Render.run(render);
 
-    // Run the engine
+    Events.on(engine, 'collisionStart', (event) => {
+      event.pairs.forEach((pair) => {
+        const { bodyA, bodyB } = pair;
+        console.log("hna ")
+        handleColision(bodyA, bodyB)
+      });
+    });
+    console.log("hamd");
 
-    // Cleanup on component unmount
-    return () => {
+    // Engine.update(engine, delta)
+    return() => {
       Render.stop(render);
-      World.remove(world, ball);
+      World.clear(engine.world , false);
       Engine.clear(engine);
-    };
-  }, []); // Run once on component mount
+
+    }
+
+  }, []);
 
   return (
     <div className='w-full h-full flex items-center justify-center'>
+      <div  className='w-2/3 h-2/3 border-2 border-black z-30' ref={boxRef}>
+        <canvas className='w-full h-full border-2 border-black z-30' id="myCanva" ref={canvasRef} />
 
-        <div className='w-2/3 h-1/2 border-2 border-black z-30	' id='gameContainer'> 
-
-        </div>
+        {/* <img ref={imgRef}  src="https://w7.pngwing.com/pngs/108/741/png-transparent-ping-pong-ball-table-amazon-com-craft-ping-pong-sphere-sports-business.png" style={{ position: 'absolute', display:'none' , top: 0, left: 0, width: 40, height: 40 }} alt="paddle" /> */}
+      </div>
     </div>
-//     <div id="gameContainer" style={
-//         {
-//             width: '800px',
-//             height: '600px',
-//             margin: 'auto',
-
-//             border: '1px solid black',
-//         }
-//     }>
-//       {/* The canvas will be rendered here */}
-//     </div>
   );
-};
-
-
+}
