3790507d824a4845151a07c9f605221db5e22f0b
anasshouarii@gmail.com <anasshouarii@gmail.com>
2023-12-24 14:01:07 +0100

    Refactor Pong page and components

--- frontend/ponje/src/app/(hamid)/Pong/VersusScreen/page.tsx
+++ frontend/ponje/src/app/(hamid)/Pong/VersusScreen/page.tsx
@@ -6,7 +6,7 @@ import Matter, { Body, Events } from 'matter-js';
 import Image from 'next/image';
 import { useEffect, useRef, useState } from "react";
 import GameResult from "../components/GameResult";
-import PlayerCard, { PlayerSkeleton } from '../components/PlayerCard';
+import PlayerCard, { PlayerSkeleton } from "../components/PlayerCard";
 import ScoreCard from "../components/ScoreCard";
 
 const socketManager = SocketManager.getInstance();
