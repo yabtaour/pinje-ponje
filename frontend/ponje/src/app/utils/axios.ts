5c69ae0368034a44ae119734e929e48be73fa522
anasshouarii@gmail.com <anasshouarii@gmail.com>
2023-12-29 23:58:38 +0100

    Remove cache control header from axios configuration

--- frontend/ponje/src/app/utils/axios.ts
+++ frontend/ponje/src/app/utils/axios.ts
@@ -4,9 +4,6 @@ const baseURL = process.env.NEXT_PUBLIC_API_URL;
 
 const axiosServices = axios.create({
   baseURL,
-  headers: {
-    "Cache-Control": "no-cache",
-  },
 });
 
 export default axiosServices;
